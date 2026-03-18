b09e2ecc86c945bb80043f61354f1f31fef2b88c
𝐌𝐑 𝐀𝐓𝐋𝐀𝐒 <afodilzr32@gmail.com>
2026-03-18 05:56:14 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports={
     recipientId : '1472234410928504832',//أيدي الشخص ألي يتحول له الكريدت عندا نسخ السيرفر
     price : '105264',//سعر النسخ
     serverid : '1466049956329820318',//أيدي سيرفارك
-    token : 'MTQ4Mjk4MDg0NDk3NjIxNDIzMA.Gpff7n.mOzZJbRjEhdBiaSb2LrdBWhp8MCCOqW0KnNOkE',//توكن بوتك
+    token : 'MTQ4Mjk4MDg0NDk3NjIxNDIzMA.GhFpvY.LVLCJePk9h_B6PapcruszPph9nQvQPBmacyZN4',//توكن بوتك
     purchaseLogChannelId: '1482784339833131070', // روم عمليات الشراء
     logsChannelId: '1482784339833131070' // روم السجلات
 }
