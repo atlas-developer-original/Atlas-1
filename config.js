237158c61baaefb58b01c5fd8a2b98ffa0bd9a1b
𝐌𝐑 𝐀𝐓𝐋𝐀𝐒 <afodilzr32@gmail.com>
2026-03-18 05:54:16 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports={
+    probot_ids :'282859044593598464',//أيدي بروبوت
+    recipientId : '1472234410928504832',//أيدي الشخص ألي يتحول له الكريدت عندا نسخ السيرفر
+    price : '105264',//سعر النسخ
+    serverid : '1466049956329820318',//أيدي سيرفارك
+    token : 'MTQ4Mjk4MDg0NDk3NjIxNDIzMA.Gpff7n.mOzZJbRjEhdBiaSb2LrdBWhp8MCCOqW0KnNOkE',//توكن بوتك
+    purchaseLogChannelId: '1482784339833131070', // روم عمليات الشراء
+    logsChannelId: '1482784339833131070' // روم السجلات
+}
